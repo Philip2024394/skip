19c5eb6f4083613d4242e993d82253fc35304359
Philip2024394 <Philip2024394@users.noreply.github.com>
2026-03-17 04:36:19 +0700

    Add Index route to AppEmergency - fix 12345 redirect to home page

--- src/AppEmergency.tsx
+++ src/AppEmergency.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
+import Index from "./pages/Index";
 import { LanguageProvider } from "@/i18n/LanguageContext";
 
 const AppEmergency = () => {
@@ -9,6 +10,8 @@ const AppEmergency = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<AuthPage />} />
+          <Route path="/home" element={<Index />} />
+          <Route path="/auth" element={<AuthPage />} />
           <Route path="/*" element={<AuthPage />} />
         </Routes>
       </BrowserRouter>
