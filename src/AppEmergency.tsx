c0cd8fd189409442f633e993c60f74c7a47225fc
Philip2024394 <Philip2024394@users.noreply.github.com>
2026-03-17 04:10:26 +0700

    Fix useLanguage error - add LanguageProvider wrapper

--- src/AppEmergency.tsx
+++ src/AppEmergency.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
+import { LanguageProvider } from "@/i18n/LanguageContext";
 
 const AppEmergency = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route path="/*" element={<AuthPage />} />
-      </Routes>
-    </BrowserRouter>
+    <LanguageProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthPage />} />
+          <Route path="/*" element={<AuthPage />} />
+        </Routes>
+      </BrowserRouter>
+    </LanguageProvider>
   );
 };
 
